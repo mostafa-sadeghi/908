b3fe74f4adbc96bff9dfe7d0ff9ddfd2aeef5149
msa <msai.sadehi@gmail.com>
2023-06-24 15:57:22 +0330

    sadeghi

---
+++ snake_game_utils.py
@@ -0,0 +1,17 @@
+from turtle import Screen, Turtle
+def make_screen():
+    main_surface = Screen()
+    main_surface.bgcolor('black')
+    main_surface.setup(width=600, height=600)
+    main_surface.title('Snake Game')
+    main_surface.tracer(False)
+    return main_surface
+
+
+def make_turtle(tshape, tcolor):
+    my_turtle = Turtle()
+    my_turtle.shape(tshape)
+    my_turtle.color(tcolor)
+    my_turtle.penup()
+    my_turtle.speed('fastest')
+    return my_turtle
